b80247c8d6d2bccfd3d1a511a806e332626858e7
Dat Nguyen <dat.nguyen@silentium.io>
2025-05-23 22:54:27 +0700

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,36 @@
 #![allow(unused_imports)]
-use std::net::TcpListener;
+use std::{io::Write, net::TcpListener};
 
 fn main() {
-    println!("Logs from your program will appear here!");
-
     let listener = TcpListener::bind("127.0.0.1:9092").unwrap();
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
+            Ok(mut _stream) => {
                 println!("accepted new connection");
+                let header = ResponseHeaderV0 {
+                    message_size: 0,
+                    correlation_id: 7,
+                };
+                let encoded_header = header.encode();
+                _stream.write_all(&encoded_header).unwrap();
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
+    }
+}
+
+struct ResponseHeaderV0 {
+    message_size: i32,
+    correlation_id: i32,
+}
+
+impl ResponseHeaderV0 {
+    fn encode(&self) -> Vec<u8> {
+        let mut buffer = Vec::new();
+        buffer.extend_from_slice(&self.message_size.to_be_bytes());
+        buffer.extend_from_slice(&self.correlation_id.to_be_bytes());
+        buffer
     }
 }
